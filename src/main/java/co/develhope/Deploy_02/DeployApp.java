6bc037d89b088a5930480b6c2694e8f370fd9e06
Jamming <j.lucreziaparrillo@gmail.com>
2024-05-14 11:47:34 +0200

    Initial commit

---
+++ src/main/java/co/develhope/Deploy_02/DeployApp.java
@@ -0,0 +1,12 @@
+package co.develhope.Deploy_02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeployApp {
+    public static void main(String[] args) {
+        SpringApplication.run(DeployApp.class, args);
+    }
+}
+
